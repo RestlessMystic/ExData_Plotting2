127ebee252e43c1f616dbc333a085d618883e471
Govind Maheswaran <govindmaheswaran@gmail.com>
2014-10-26 13:25:01 +0530

    Plot 2 Completed

--- Plot.R
+++ Plot.R
@@ -1,9 +1,9 @@
 # Read Data
 data <- readRDS("summarySCC_PM25.rds")
 code <- readRDS("Source_Classification_Code.rds")
-
+data2 <- subset(data, data$fips == 24510)
 # Preparing data
-sumvec <- rowsum(data$Emissions, data$year)
+sumvec <- rowsum(data2$Emissions, data2$year)
 new.df <- cbind.data.frame(rownames(sumvec), sumvec)
 colnames(new.df) <- c("Year", "Emission")
 
